eba369592242c115b51cfb4ccfaa7b85415f5751
987sebasBeller <sebastianbeller76@gmail.com>
2024-08-26 01:22:11 -0400

    Cells audio was added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const OUTSIDE_SHADOW_CLASS = "outside-shadow";
 const INSIDE_SHADOW_CLASS = "inside-shadow";
 const DELAY = 200;
 const DELAY_CELL_EFFECT = 1000;
+const WRONG_AUDIO_ID="wrong";
 
 let level;
 let game_over = true;
@@ -36,16 +37,22 @@ function effect_remove_class_from_cell(cell, class_name) {
   });
 }
 
+function play_audio(audio_id){
+  let audio = new Audio(`sounds/${audio_id}.mp3`);
+  audio.play();
+}
 function update_level() {
   $("h2").text(`Level: ${level++}`);
 }
 async function play_effect_for_cell(cell_id, cell_class) {
+  if(game_over) return;
   $(`.${cell_id}`).addClass(cell_class);
   await effect_remove_class_from_cell(cell_id, cell_class);
 }
 async function show_sequence() {
   for (let cell of sequence) {
     await play_effect_for_cell(cell, OUTSIDE_SHADOW_CLASS);
+    play_audio(cell);
     await play_effect_for_cell(cell, INSIDE_SHADOW_CLASS);
   }
 }
@@ -70,7 +77,6 @@ function remove_game_over_state() {
 function show_game_over_state() {
   $("h2").text("Game Over, Press Any Key to Restart");
   $("body").addClass("game-over");
-  remove_game_over_state();
 }
 
 const is_the_same_sub_sequence = () => {
@@ -80,10 +86,14 @@ const is_the_same_sub_sequence = () => {
 const is_the_same_sequence_length = () =>
   user_sequence.length == sequence.length;
 function check_game_over(event) {
-  user_sequence.push(event.target.classList[1]);
+  let cell=event.target.classList[1]
+  user_sequence.push(cell);
+  play_audio(cell);
   if (!is_the_same_sub_sequence()) {
     game_over = true;
     show_game_over_state();
+    remove_game_over_state();
+    play_audio(WRONG_AUDIO_ID);
     return;
   }
   if (is_the_same_sequence_length()) {
@@ -94,3 +104,4 @@ function check_game_over(event) {
 
 
 $(".button-grid").on("click", check_game_over);
+
